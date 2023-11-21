10ba41bb41beed4a2be66e6592ce47841174f14d
Amulya Jain <amuly.jain@gmail.com>
2023-11-21 00:14:26 +0000

    Formated

--- helloworld.java
+++ helloworld.java
@@ -1,27 +1,22 @@
-class helloworld
-{
-    public static void main(String[] args) 
-    {
-        int counter=0;
+class helloworld {
+    public static void main(String[] args) {
+        int counter = 0;
         String s1 = "Hello World";
-        String ans= "";
-        for(int i=0;i<s1.length();i++)
-        {
-            boolean flag=true;
-            while (flag)
-            {
+        String ans = "";
+        for (int i = 0; i < s1.length(); i++) {
+            boolean flag = true;
+            while (flag) {
                 counter++;
-                char c =(char)((Math.random()*91)+32);
-                if(c==s1.charAt(i))
-                {
-                    ans+=c;
-                    flag=false;
+                char c = (char) ((Math.random() * 91) + 32);
+                if (c == s1.charAt(i)) {
+                    ans += c;
+                    flag = false;
                     System.out.println(ans);
                     continue;
                 }
-                System.out.println(ans+c);
+                System.out.println(ans + c);
             }
         }
-        System.out.println("No of guesses "+counter);
+        System.out.println("No of guesses " + counter);
     }
 }
